a31c2581cc8466ce2cab8c2a8a29699725d37657
khurley <khurley@ks-macbook-pro.myfiosgateway.com>
2024-09-01 00:22:22 -0400

    test

--- NYFA_jobalerts.py
+++ NYFA_jobalerts.py
@@ -50,13 +50,21 @@ def send_email(subject, body):
 # Main function
 def main():
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument("--headless=new")  # Use a more realistic headless mode
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.binary_location = "/usr/bin/google-chrome"  # Specify the path to the Chrome binary
     options.add_argument(f"user-agent={user_agent}")
+    options.add_argument("window-size=1920x1080")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--enable-gpu")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
 
     driver = webdriver.Chrome(options=options)
+
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     
     job_alert = check_jobs(driver)
     if job_alert:
